d434eac8716164d20ab38d2e74e99e0a7d7b77be
Hunter <hunterwiley88@gmail.com>
2018-08-07 14:45:20 -0700

    post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport'); 
+
+// Post Model
+const Post = require('../../models/Post');
+// Profile Model
+const Profile = require('../../models/Profile');
+
+const validatePostInput = require('../../validation/post')
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Post works!" }));
+
+// @route GET api/posts test
+// @desc Tests post route
+// @access Public
 
 module.exports = router;
